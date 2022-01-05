d7cf7d86aa152db069d1ff468e092274cf6815ef
tonisuwendi <tonisuwendi2002@gmail.com>
2022-01-05 16:47:39 +0700

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -32,7 +32,7 @@ export async function getStaticPaths() {
   const meetups = await meetupsCollections.find({}, { _id: 1 }).toArray();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: {
         meetupId: meetup._id.toString(),
